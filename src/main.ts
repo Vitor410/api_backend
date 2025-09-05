70ec00e492b42fd5ef16fd062907556ea3ea4640
Vitor410 <vitor.rossi.scholl@gmail.com>
2025-09-05 11:34:34 -0300

    projetoAPI

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,16 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }));
+
+  // Habilita o CORS para que seu front-end possa acessar a API
+  app.enableCors({
+    origin: 'http://localhost:5173', // A URL do seu front-end (Vite usa 5173 por padrão)
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
+ 
